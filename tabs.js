9f8b7da4434dbb16ca2b9b4a234ddeb78a9e7011
Apprentice <apprentice@dbc09.local>
2013-08-13 10:44:27 -0700

    finish tab ex

--- tabs.js
+++ tabs.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  // Put your JavaScript behavior here
+  // on click
+
+  $('.tabs a').on('click', function() {
+      // style the button
+      $('.tabs li').removeClass('active');
+      $(this).parent('li').addClass('active');
+
+      // show the right tab content
+      $('.tab').hide();
+      var tab_selector = $(this).attr('href');
+      $(tab_selector).show();
+  });
+
+
 });
